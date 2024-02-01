f1887559cded552f731244272379ff19d784d8eb
Shashank Siripragada <siripragada.s@northeastern.edu>
2024-02-01 01:18:19 -0500

    Update AddObjectScript.py

---
+++ AddObjectScript.py
@@ -0,0 +1,33 @@
+import bpy
+
+
+class TestPanel(bpy.types.Panel):
+    bl_label = "Test Panel"
+    bl_idname = "PT_TestPanel"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = 'UI'
+    
+    bl_category = 'NewTab'
+    
+    
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row()
+        row.label(text="Add an object", icon='CUBE')
+        row.operator("mesh.primitive_cube_add")
+        
+        
+
+
+def register():
+    bpy.utils.register_class(TestPanel)
+    
+def unregister():
+    bpy.utils.unregister_class(TestPanel)
+    
+
+if __name__ == '__main__':
+    register()
+        
+    
+    
